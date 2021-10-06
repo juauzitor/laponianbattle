f8dfe9e838c02ad7ffecac9cfd48ed6afbc05538
juauzitor <joao.vitor.mendes.pinto@gmail.com>
2021-10-05 21:42:39 -0300

    0.0.2-board&fixup

--- canvas.h
+++ canvas.h
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define  LINE 13
+#define  ROW 13
 
-void start_board(void){
-    char board[LINE][ROW]= {
+
+// Funções para gerenciar o tabuleiro
+typedef struct{
+    char board[LINE][ROW];
+} tp_board;
+
+void start_board(tp_board *b){
+    char scene[LINE][ROW]= {
         {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', ' ', ' '},
         {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', ' '},
         {'*', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '*', '1'},
@@ -16,24 +25,58 @@ void start_board(void){
         {'*', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '*', 'A'},
         {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', ' '}
     };
+
+    for(int i=0; i<LINE; i++){
+        for (size_t j = 0; j < ROW; j++) {
+            b->board[i][j] = scene[i][j];
+        }
+    }
+
 }
 
+// Funções para gerenciar a tela
 
-/*void print_board(char **board){
+void print_board(tp_board *b){
     for(int i=0; i<LINE; i++){
         for(int j=0; j<ROW; j++){
             if(j==ROW-1){
-                if(board[i][j]=='A'){
-                    printf("| 10");
+                if(b->board[i][j]=='A'){
+                    printf(" | 10 ");
+                } else {
+                printf(" | %c ", b->board[i][j]);
                 }
-                printf("| %c", board[i][j]);
-            } else if(board[i][j] == '.'){
-                printf(" ");
+            } else if(b->board[i][j] == '.'){
+                printf("   ");
             } else{
-                printf("%c", board[i][j]);
+                printf(" %c ", b->board[i][j]);
             }
         }
         printf("\n");
     }
 }
-*/
+
+void print_name(char *fp, char *sp){
+    printf("Norte: %s\n", fp);
+    printf("Sul: %s\n", sp);
+}
+
+void clear(void){
+// Windows
+    #ifdef WIN32
+        system("cls");
+// Linux/unix
+    #else
+        system("clear");
+    #endif
+}
+
+void pause(void){
+// Windows
+    #ifdef WIN32
+        system("pause");
+// Linux/unix
+    #else
+        printf("Press any key to continue ..\n");
+        system("bash -c 'read -n1 -r -p '...' key'");
+    #endif
+}
--- laponia.c
+++ laponia.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-#include "board.h"
-
-
-#define LINE 13
-#define ROW 13
+#include "canvas.h"
+#define MAX 200
 
 
 int main(int argc, char *argv[]){
-    start_board();
+    tp_board game_board;
+    char first_player[MAX], second_player[MAX];
 
+    printf("Nome do primeiro jogador: ");
+    scanf(" %[^\n]s", first_player);
+    printf("Nome do segundo jogador: ");
+    scanf(" %[^\n]s", second_player);
+
+    start_board(&game_board);
+    print_board(&game_board);
+    clear();
+    print_name(first_player, second_player);
+    print_board(&game_board);
+    pause();
 
     return 0;
 }
