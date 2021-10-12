8cb79bdaec4cbc45592b3c8f36958b725ec7856f
juauzitor <joao.vitor.mendes.pinto@gmail.com>
2021-10-12 12:51:43 -0300

    0.1.1 - bugfix in alloc_unit and a new feature to move the unit

--- laponia.c
+++ laponia.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[]){
     tp_board game_board;
     tp_stack first_player_pieces, second_player_pieces;
     tp_queue q1;
-    char first_player[NAME], second_player[NAME];
+    int t, line, row;
+    char first_player[NAME], second_player[NAME], option;
     start_stack(&first_player_pieces);
     start_stack(&second_player_pieces);
     start_queue(&q1);
     start_turn(&q1);
     start_board(&game_board);
     add_pieces(&first_player_pieces, &second_player_pieces);
-
     printf("Nome do primeiro jogador: ");
     scanf(" %[^\n]s", first_player);
     printf("Nome do segundo jogador: ");
@@ -30,6 +30,41 @@ int main(int argc, char *argv[]){
     print_name(first_player, second_player);
     print_board(&game_board);
     pause();
+    while (1) {
+        t = turn(&q1);
+        printf("Você deseja atacar ou mover, digite a para ataque ou m para mover.\n");
+        scanf(" %c", &option);
+        printf("line\n");
+        scanf("%d", &line);
+        printf("row\n");
+        scanf("%d", &row);
+        if (t == 1) {
+            if(option == 'm'){
+                printf("Para qual direção:\n");
+                scanf(" %c", &option);
+                first_player_move_unit(&game_board, line, row, &option);
+                pause();
+            } else if(option == 'a'){
 
+            } else {
+                printf("Você jogou errado e perdeu a vez!\n");
+                pause();
+            }
+        } else{
+            if(option == 'm'){
+                printf("Para qual direção:\n");
+                scanf(" %c", &option);
+                second_player_move_unit(&game_board, line, row, &option);
+                pause();
+            } else if(option == 'a'){
+
+            } else {
+                printf("Você jogou errado e perdeu a vez!\n");
+                pause();
+            }
+        }
+        clear();
+        print_board(&game_board);
+    }
     return 0;
 }
--- rules.h
+++ rules.h
@@ -16,20 +16,60 @@ void alloc_unit(tp_stack *p1, tp_stack *p2, tp_board *b, tp_queue *q){
     while (!empty_stack(p1) || !empty_stack(p2)) {
         int t = turn(q), line, row, e;
         printf("linha\n");
-        scanf("%d\n", &line);
+        scanf("%d", &line);
         printf("coluna\n");
-        scanf("%d\n", &row);
+        scanf("%d", &row);
         if (t == 1 && line < 6 && line > 1 && row > 0 && row < 12) {
             pop(p1, &e);
             b->board[line][row] = e;
             clear();
             print_board(b);
-        } else if (t == 0 && line > 7 && line < 12 && row > 0 && row < 12){
+        } else if (t == 2 && line > 7 && line < 12 && row > 0 && row < 12){
             pop(p2, &e);
             b->board[line][row] = e;
+            clear();
             print_board(b);
         } else {
             printf("jogada ilegal\n");
         }
     }
+}
+
+void first_player_move_unit(tp_board *b, int line, int row, char *m){
+    if(*m == 'f'){
+        b->board[line+1][row] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else if(*m == 'b'){
+        b->board[line-1][row] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else if(*m == 'r'){
+        b->board[line][row+1] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else if(*m == 'l'){
+        b->board[line][row-1] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else {
+        printf("está errado\n");
+    }
+}
+
+void second_player_move_unit(tp_board *b, int line, int row, char *m){
+    if(*m == 'f'){
+        b->board[line-1][row] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else if(*m == 'b'){
+        b->board[line+1][row] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else if(*m == 'r'){
+        b->board[line][row-1] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else if(*m == 'l'){
+        b->board[line][row+1] = b->board[line][row];
+        b->board[line][row] = '.';
+    } else {
+        printf("está errado\n");
+    }
+}
+
+void attack_unit(tp_board *b, int *line, int *row, char *m){
 }
